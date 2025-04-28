dccd0fe0aedf104476a77067e76f65eb07cf6e2f
나일성 <ilseong.na@woowahan.com>
2025-04-28 19:19:02 +0900

    [feat] 카프카 이벤트 발행을 비동기로 변경
    Co-authored-by: 나일성 <ilseongdev@gmail.com>
    Co-authored-by: 김범수 <kbsat@naver.com>

--- src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
+++ src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
@@ -5,7 +5,6 @@ import com.commerce.flashsale.message.OrderEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,13 +22,14 @@ public class OrderEventProducer {
                 .productName(productName)
                 .success(success)
                 .build();
-            SendResult<String, Object> stringStringSendResult = kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
-                .get();
-            log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
-                stringStringSendResult,
-                stringStringSendResult.getRecordMetadata().topic(),
-                stringStringSendResult.getRecordMetadata().partition(),
-                stringStringSendResult.getRecordMetadata().offset());
+            kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
+                .thenAccept(stringStringSendResult -> {
+                    log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
+                        stringStringSendResult,
+                        stringStringSendResult.getRecordMetadata().topic(),
+                        stringStringSendResult.getRecordMetadata().partition(),
+                        stringStringSendResult.getRecordMetadata().offset());
+                });
         } catch (Exception e) {
             log.error("카프카 메시지 발행 실패 - 예외: {}", e.getMessage(), e);
             throw new RuntimeException("카프카 메시지 발행 중 오류 발생: " + e.getMessage(), e);
