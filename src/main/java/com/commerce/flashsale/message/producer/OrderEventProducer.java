54f0d08fb3069ac5448118dac25c73d2f0cf45f2
나일성 <ilseong.na@woowahan.com>
2025-04-28 19:45:02 +0900

    [feat] 이벤트 발행을 멀티 스레드로 실행
    Co-authored-by: 나일성 <ilseongdev@gmail.com>
    Co-authored-by: 김범수 <kbsat@naver.com>

--- src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
+++ src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
@@ -7,32 +7,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class OrderEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ExecutorService executorService;
 
     public void produceMessage(String uuid, String productName, boolean success) {
-        try {
-            log.info("카프카 메시지 발행 시작 - 토픽: {}, 메시지: {}", KafkaConfig.TOPIC_NAME, success);
-            OrderEvent event = OrderEvent.builder()
-                .uuid(uuid)
-                .productName(productName)
-                .success(success)
-                .build();
-            kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
-                .thenAccept(stringStringSendResult -> {
-                    log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
-                        stringStringSendResult,
-                        stringStringSendResult.getRecordMetadata().topic(),
-                        stringStringSendResult.getRecordMetadata().partition(),
-                        stringStringSendResult.getRecordMetadata().offset());
-                });
-        } catch (Exception e) {
-            log.error("카프카 메시지 발행 실패 - 예외: {}", e.getMessage(), e);
-            throw new RuntimeException("카프카 메시지 발행 중 오류 발생: " + e.getMessage(), e);
-        }
+
+        executorService.submit(() -> {
+            try {
+                log.info("카프카 메시지 발행 시작 - 토픽: {}, 메시지: {}", KafkaConfig.TOPIC_NAME, success);
+                OrderEvent event = OrderEvent.builder()
+                    .uuid(uuid)
+                    .productName(productName)
+                    .success(success)
+                    .build();
+                kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
+                    .thenAccept(stringStringSendResult -> {
+                        log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
+                            stringStringSendResult,
+                            stringStringSendResult.getRecordMetadata().topic(),
+                            stringStringSendResult.getRecordMetadata().partition(),
+                            stringStringSendResult.getRecordMetadata().offset());
+                    });
+            } catch (Exception e) {
+                log.error("카프카 메시지 발행 실패 - 예외: {}", e.getMessage(), e);
+                throw new RuntimeException("카프카 메시지 발행 중 오류 발생: " + e.getMessage(), e);
+            }
+        });
+
+
     }
 }
