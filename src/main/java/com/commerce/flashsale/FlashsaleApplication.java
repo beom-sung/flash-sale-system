e3403dd8809c7f759cca1afe3d232d9f03335b6c
kbs <kbs@woowahan.com>
2025-05-08 20:56:30 +0900

    [feat] 메시지 발송 Async 애노테이션으로 비동기처리 진행
    Co-authored-by: 나일성 <ilseongdev@gmail.com>
    Co-authored-by: 김범수 <kbsat@naver.com>

--- src/main/java/com/commerce/flashsale/FlashsaleApplication.java
+++ src/main/java/com/commerce/flashsale/FlashsaleApplication.java
@@ -2,8 +2,10 @@ package com.commerce.flashsale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FlashsaleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
+++ src/main/java/com/commerce/flashsale/message/producer/OrderEventProducer.java
@@ -5,9 +5,8 @@ import com.commerce.flashsale.message.OrderEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.ExecutorService;
 
 @Service
 @Slf4j
@@ -15,32 +14,27 @@ import java.util.concurrent.ExecutorService;
 public class OrderEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final ExecutorService executorService;
 
-    public void produceMessage(String uuid, String productName, boolean success) {
-
-        executorService.submit(() -> {
-            try {
-                log.info("카프카 메시지 발행 시작 - 토픽: {}, 메시지: {}", KafkaConfig.TOPIC_NAME, success);
-                OrderEvent event = OrderEvent.builder()
-                    .uuid(uuid)
-                    .productName(productName)
-                    .success(success)
-                    .build();
-                kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
-                    .thenAccept(stringStringSendResult -> {
-                        log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
-                            stringStringSendResult,
-                            stringStringSendResult.getRecordMetadata().topic(),
-                            stringStringSendResult.getRecordMetadata().partition(),
-                            stringStringSendResult.getRecordMetadata().offset());
-                    });
-            } catch (Exception e) {
-                log.error("카프카 메시지 발행 실패 - 예외: {}", e.getMessage(), e);
-                throw new RuntimeException("카프카 메시지 발행 중 오류 발생: " + e.getMessage(), e);
-            }
-        });
-
-
+    @Async
+    public void produceMessage(String uuid, String productName, boolean success)  {
+        try {
+            log.info("카프카 메시지 발행 시작 - 토픽: {}, 메시지: {}", KafkaConfig.TOPIC_NAME, success);
+            OrderEvent event = OrderEvent.builder()
+                .uuid(uuid)
+                .productName(productName)
+                .success(success)
+                .build();
+            kafkaTemplate.send(KafkaConfig.TOPIC_NAME, event)
+                .thenAccept(stringStringSendResult -> {
+                    log.info("카프카 메시지 발행 성공 - 결과: {}, 토픽: {}, 파티션: {}, 오프셋: {}",
+                        stringStringSendResult,
+                        stringStringSendResult.getRecordMetadata().topic(),
+                        stringStringSendResult.getRecordMetadata().partition(),
+                        stringStringSendResult.getRecordMetadata().offset());
+                });
+        } catch (Exception e) {
+            log.error("카프카 메시지 발행 실패 - 예외: {}", e.getMessage(), e);
+            throw new RuntimeException("카프카 메시지 발행 중 오류 발생: " + e.getMessage(), e);
+        }
     }
 }
